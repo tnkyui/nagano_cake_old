cba36288021dc41af86299328407cd9dfbc352b6
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-06-30 17:32:39 +0000

    [Update]rootpath

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  
   devise_for :admin_users, path: 'admin'
   devise_for :customers
+
+  root to: 'public/homes#top'
+
+  namespace :admin do
+    root to: 'homes#top'
+  end
+
+
   namespace :admin do
     get 'order_details/update'
   end
@@ -28,45 +37,41 @@ Rails.application.routes.draw do
     get 'items/edit'
     get 'items/update'
   end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
+
     get 'addresses/index'
     get 'addresses/edit'
     get 'addresses/create'
     get 'addresses/update'
     get 'addresses/destroy'
-  end
-  namespace :public do
+
+
     get 'orders/new'
     get 'orders/confirm'
     get 'orders/complete'
     get 'orders/create'
     get 'orders/index'
     get 'orders/show'
-  end
-  namespace :public do
+
+
     get 'cart_items/index'
     get 'cart_items/update'
     get 'cart_items/destroy'
     get 'cart_items/destroy_all'
     get 'cart_items/create'
-  end
-  namespace :public do
+
+
     get 'customers/show'
     get 'customers/edit'
     get 'customers/update'
     get 'customers/confirm'
     get 'customers/withdraw'
-  end
-  namespace :public do
+
+
     get 'items/index'
     get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
+
+
     get 'homes/about'
-  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
