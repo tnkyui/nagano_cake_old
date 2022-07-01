49c494b4284c71d7ca911a90201c70d839ecc506
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-07-01 16:10:05 +0000

    [Update]admin after_sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      user_root_path
+    when AdminUser
+      admin_root_path
+    end
+  end
 end
